77000468445e7ab87f8a20221f30a99c9463675d
Tofan Adi Nugroho <94297488+DifaGro@users.noreply.github.com>
2024-12-19 15:34:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ from pathlib import Path
 
 # Tentukan path file secara dinamis
 base_dir = Path(__file__).resolve().parent
-model_path = base_dir / "lung_cancer_sgd_model.pkl"
-scaler_path = base_dir / "scaler.pkl"
-
-# Muat model dan scaler
-try:
-    model = joblib.load(model_path)
-    scaler = joblib.load(scaler_path)
-except FileNotFoundError:
-    st.error("Model atau scaler tidak ditemukan. Pastikan file berada di folder 'model'.")
-    st.stop()
+model = base_dir / "lung_cancer_sgd_model.pkl"
+scaler = base_dir / "scaler.pkl"
 
 # Tampilan aplikasi
 st.markdown("<h2>Prediksi Kanker Paru-Paru - Menggunakan Stochastic Gradient Descent</h2>", unsafe_allow_html=True)
